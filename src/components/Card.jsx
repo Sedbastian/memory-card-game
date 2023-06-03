859dafb73a677dcd9f14bf04ab89a16a678b2bc6
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2023-06-03 11:37:17 -0300

    Add turn effect on cards after each click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./styles/Card.css";
 
 export default function Card({ card, addClickedCard }) {
+  const cardRef = useRef(null);
+  useEffect(() => {
+    const ref = cardRef.current;
+    function removeEffectClass() {
+      ref.classList.remove("turn");
+    }
+    ref.addEventListener("animationend", removeEffectClass);
+    ref.classList.add("turn");
+
+    return () => {
+      ref.removeEventListener("animationend", removeEffectClass);
+      ref.classList.remove("turn");
+    };
+  });
+
   return (
     <img
+      ref={cardRef}
       className="card"
       alt="Van Gogh"
       src={card}
